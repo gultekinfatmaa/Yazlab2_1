1102a027fd778267deaf5ccf06573907a66dc504
fatma <fatma@192.168.1.24>
2021-03-30 20:54:33 +0300

    4.asama eklendi

--- DemoMavenEclipseProject/src/main/java/ToolsQA/DemoMavenEclipseProject/App.java
+++ DemoMavenEclipseProject/src/main/java/ToolsQA/DemoMavenEclipseProject/App.java
@@ -102,7 +102,15 @@ class App
 	        	counter=1;
 	    	}
 	    }
-	    //frekansı en yüksek 10 kelimeyi buluyor
+	    /*
+	    System.out.println("\n FREKANSLAR\n");
+	    for(int i=0;i<CounterList.size();i++) {
+	    	System.out.println("\nt"+i+" "+WordList.get(i)+" "+CounterList.get(i));
+	    }
+	    */
+	    
+	    //frekansı en yüksek 20 kelimeyi buluyor
+	    
 	    int flag=0;	    
 	    int bigger=CounterList.get(0);
 	    
@@ -121,16 +129,13 @@ class App
 	    	flag=0;
 	    }
 	    
-	    //frekansı en yüksek 10 kelimeyi yazdırıyor
+	    //frekansı en yüksek 20 kelimeyi yazdırıyor
+	    System.out.println("\n\n 20 ANAHTAR KELİME");
 	    for(int i=0;i<biggestWords.size();i++) {
-	    	System.out.println("\nt"+i+" "+biggest.get(i)+" "+biggestWords.get(i));
+	    	System.out.println(" "+biggestWords.get(i)+" "+biggest.get(i));
 	    }
 	    return biggestWords;
 	}
-	
-	
-	
-	
 	
 	
 	
@@ -139,39 +144,99 @@ class App
     	List<String> biggestWords=new ArrayList<String>();
     	List<String> biggestWords2=new ArrayList<String>();
 	    Scanner input=new Scanner(System.in);
+	    Scanner put=new Scanner(System.in);
 	    
-	    System.out.println("Input URL:");
-	    String a=input.nextLine();
-	    
-	    biggestWords=keywords(a);
-	    
+	    System.out.println("1. asama için 1 \n2. ve 3. asama icin 2 \n 4.asama için 4");
+	    int asama=put.nextInt();
+	    switch (asama) {
+	    	case 1:
+	    		System.out.println("Input URL:");
+	    		String a=input.nextLine();
+    	    
+	    		biggestWords=keywords(a);
+	    		break;
+	    	case 2:
+	    		System.out.println("Input URL:");
+	    		String c=input.nextLine();
+	    		biggestWords=keywords(c);
+	    		
+	    		System.out.println("Input URL:");
+	    	    String b=input.nextLine();
+	    	    biggestWords2=keywords(b);
+	    	    
+	    	  //benzerlik
+	    	    int flag=0;
+	    	    for(int i=0;i<biggestWords.size();i++) {
+	    	    	if(biggestWords2.contains(biggestWords.get(i))) {
+	    	    		System.out.println("benzer kelime "+biggestWords.get(i));
+	    	    		flag++;
+	    	    	}
+	    	    }
+	    	    System.out.println(c+" ve "+b+" arasındaki benzerlik oranı %"+(flag*500)/100);
+	    	    break;
+	    	case 4:
+	    		
+	    	    List<String> fileUrl=new ArrayList<String>();
+	    	    List<Integer> benzerlik=new ArrayList<Integer>();
+	        	List<String> biggestWords4=new ArrayList<String>();
+	        	int tutucu=0;
+	    		System.out.println("Input URL:");
+	    	    String d=input.nextLine();
+	    	    biggestWords4=keywords(d);
+	    		File f = new File("C:\\\\Users\\\\fatma\\\\source\\\\repos\\\\DemoMavenEclipseProject\\\\url.txt"); //sıralanacak urller
+	    		Scanner scanner = new Scanner(f);
+	    	    
+	    	    //ayıklanacak bağlaçlar dosyadan çekiliyor
+	    	    for(int i=0;scanner.hasNext();i++) {
+	    	    	fileUrl.add(i, scanner.nextLine());
+	    	    }
+	    	    int urlNum=fileUrl.size();
+	    	    ArrayList<ArrayList<String>> words = new ArrayList<>(fileUrl.size());
+	    	    for(int i=0;i<fileUrl.size();i++) {
+	    	    	words.get(i).addAll(i, keywords(fileUrl.get(i)));
+	    	    			//keywords(fileUrl.get(i));
+	    	    }
+	    	    
+	    	    for(int i=0;i<urlNum;i++) {
+	    	    	for(int j=0;j<biggestWords4.size();j++) {
+	    	    		if(biggestWords4.contains(words.get(i).get(j))) {
+	    	    			tutucu++;
+	    	    		}
+	    	    	}
+	    	    	benzerlik.add(i,tutucu);
+	    	    }
+	    	    int bb=0;
+	    	    int enBenzer=benzerlik.get(0);
+	    	    for(int i=0;i<benzerlik.size();i++) {
+	    	    	for(int j=1;j<benzerlik.size();j++) {
+	    	    		if(benzerlik.get(j)>enBenzer) {
+	    	    			enBenzer=benzerlik.get(j);
+	    	    			bb=j;
+	    	    		}
+	    	    	}
+	    	    }
+	    	    System.out.println("\nSıralama birincisi: "+fileUrl.get(bb));
+	    	    
+	    		break;
+	    	
+	    }
+	    	
 	    //    https://en.wikipedia.org/wiki/German_Workers%27_Party
 	    //	  https://en.wikipedia.org/wiki/Adolf_Hitler
-	    //frequency
 	    
 	    System.out.println("\n------------------------------------");
+	    /*
 	    System.out.println("\n"+a+" için nahtar kelimeler:");
 	    for(int i=0;i<biggestWords.size();i++) {
 	    	System.out.println(" "+biggestWords.get(i));
 	    }
+	    */
 	    
-	    System.out.println("Input URL:");
-	    String b=input.nextLine();
-	    biggestWords2=keywords(b);
+	    /*
 	    System.out.println("\n"+b+" için nahtar kelimeler:");
 	    for(int i=0;i<biggestWords2.size();i++) {
 	    	System.out.println(" "+biggestWords2.get(i));
-	    }
-	    
-	    //benzerlik
-	    int flag=0;
-	    for(int i=0;i<biggestWords.size();i++) {
-	    	if(biggestWords2.contains(biggestWords.get(i))) {
-	    		System.out.println("\nbenzer kelime "+biggestWords.get(i));
-	    		flag++;
-	    	}
-	    }
-	    System.out.println(a+" ve "+b+" arasındaki benzerlik oranı %"+(flag*500)/100);
+	    }*/
     }
 }
 /*public int similarity(List<Integer> biggest,List<Integer> CounterList) {
